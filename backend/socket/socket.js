78f749c0cf414aea22c7fe4efc9749e5621e98d7
Harshal006-rgb <harshalparanjiya@gmail.com>
2026-07-10 02:19:34 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors:{
-        origin: ["http://localhost:5173"],
+        origin: ["https://spill-the-tea-4251.onrender.com"],
         methods: ["GET", "POST"]
     }
 })
